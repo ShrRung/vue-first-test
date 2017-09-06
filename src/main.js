316db0d39a9bd50920e806eda24d4f161a6f174c
ShrRung <1026825079@qq.com>
2017-09-06 22:49:55 +0800

    layout setp one

--- src/main.js
+++ src/main.js
@@ -1,21 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import Layout from './components/layout'
 import VRouter from 'vue-router'
 
 Vue.use(VRouter)
 
 let router = new VRouter({
-
+    mode: 'history',
+    routes:[
+        {
+            path: '/',
+            component:IndexPage
+        }
+    ]
 })
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
-    template: '<App/>',
-    components: { App }
+    template: '<Layout/>',
+    components: { Layout }
 })
