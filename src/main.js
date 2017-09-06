fb34e00a7cc47ff9f6a4904f25b25c03d5a377ee
ShrRung <1026825079@qq.com>
2017-09-06 15:40:34 +0800

    安装vue-router

--- src/main.js
+++ src/main.js
@@ -2,12 +2,20 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VRouter from 'vue-router'
+
+Vue.use(VRouter)
+
+let router = new VRouter({
+
+})
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: { App }
 })
